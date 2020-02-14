4688a4d07f01de5e50c549d808086d86d9d31bb2
bottleneck2020 <fullstacktorino@gmail.com>
2020-02-14 17:20:31 +0100

    Installing and adding Bootstrap style

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do 
 
   resources :articles
-	root 'pages#homepage'
+	root 'pages#home'
 	get 'about', to: 'pages#about'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
